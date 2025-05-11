acdbf270203164e695159efc6f02f3932610fd45
Stella-11 <selotastar@gmail.com>
2025-05-11 16:34:44 +0200

    S.M Selota 219447000
    Session beans for entity classes

---
+++ ShopEJBModule/src/java/za/ac/tut/bl/ProductFacadeLocal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.bl;
+
+import java.util.List;
+import javax.ejb.Local;
+import za.ac.tut.entities.Product;
+
+/**
+ *
+ * @author downtown
+ */
+@Local
+public interface ProductFacadeLocal {
+
+    void create(Product product);
+
+    void edit(Product product);
+
+    void remove(Product product);
+
+    Product find(Object id);
+
+    List<Product> findAll();
+
+    List<Product> findRange(int[] range);
+
+    int count();
+    
+}
