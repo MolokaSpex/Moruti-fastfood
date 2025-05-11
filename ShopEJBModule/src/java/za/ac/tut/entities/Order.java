e4d8f5f91b7d67b803f75c2d4b5c1a976ba8b1c7
She's Brilliant <150003403+BrilliantMatlala@users.noreply.github.com>
2025-05-11 15:06:49 +0200

    Brilliant 221857887
    Order Entity

---
+++ ShopEJBModule/src/java/za/ac/tut/entities/Order.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.tut.model.entities;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Brilliant
+ */
+@Entity
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Integer customer_id;//fk
+    private String order_status;
+    private double total_amount;
+
+    public Order() {
+    }
+
+    public Order(String order_status, double total_amount) {
+        this.order_status = order_status;
+        this.total_amount = total_amount;
+    }
+
+    public Order(Integer customer_id, String order_status, double total_amount) {
+        this.customer_id = customer_id;
+        this.order_status = order_status;
+        this.total_amount = total_amount;
+    }
+
+    public Integer getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(Integer customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getOrder_status() {
+        return order_status;
+    }
+
+    public void setOrder_status(String order_status) {
+        this.order_status = order_status;
+    }
+
+    public double getTotal_amount() {
+        return total_amount;
+    }
+
+    public void setTotal_amount(double total_amount) {
+        this.total_amount = total_amount;
+    }
+    
+    
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Order)) {
+            return false;
+        }
+        Order other = (Order) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "za.ac.tut.model.entities.Order[ id=" + id + " ]";
+    }
+    
+}
