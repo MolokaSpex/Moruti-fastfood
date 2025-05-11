e62f12d425973eec5146deb85c28912d21624fba
Ellias <themaellias@gmail.com>
2025-05-11 14:47:03 +0200

    E.Thema 219549644
    Skeleton Update

---
+++ ShopEJBModule/src/java/za/ac/tut/model/ImageManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.model;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author thema
+ */
+public class ImageManager implements ImageInterface{
+
+    @Override
+    public byte[] getByteImage(String img) {
+        Path imgPath = Paths.get(img);
+        byte[] imageBytes = null;
+        try {
+            imageBytes = Files.readAllBytes(imgPath);
+        } catch (IOException e) {
+            System.err.println("Error reading image file: " + e.getMessage());
+        // You might want to throw a custom exception or return null here
+        }
+        return imageBytes;
+    }
+    
+}
